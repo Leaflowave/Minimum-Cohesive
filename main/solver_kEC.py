ed26c5d5f88a92af66334c5a4ef05d8c55689a13
Leaflowave <22725007+Leaflowave@users.noreply.github.com>
2023-06-05 15:17:28 +1200

    Update and rename solver_kEC.py to main/solver_kEC.py

---
+++ main/solver_kEC.py
@@ -0,0 +1,116 @@
+import random
+import time
+import networkx as nx
+import copy
+from Chang1 import KECC, subgraph
+
+def MinimalSubgraph(G, k, d, X, count=None):
+    """
+    implementation of the Mincoh algorithm on k-EC index
+    :param G: a k-EC graph
+    :param k: cohesiveness level
+    :param d: record of total number of randomly removed vertices
+    :param X: query set
+    :param count: the value r, meaning the num of randomly sampled vertices each step
+    :return: a small k-EC subgraph
+    """
+    red=[]
+    ansg = G
+    step = 0
+    anssize = len(ansg)
+    ansgNodes = set(ansg.nodes())
+    redSet = set(red)
+    while step < 4 and len(ansgNodes - redSet - X) > 0:
+        anssize = len(ansg)
+        g = copy.deepcopy(ansg)
+        if count is None: count = max(len(g) // (k + 1), 2)
+        while count > len(ansgNodes - set(red) - set(X)):
+            count = max(count // 2, 2)
+        S = random.sample(list(ansgNodes - redSet - X), count)  # random sampling
+        redSet.update(S)
+        red.extend(S)
+        g.remove_nodes_from(S)
+
+        vccs = KECC(g, k, X)
+        if len(vccs) >= 1:
+            step = 0
+            for vcc in vccs:
+                if len(X - vcc) > 0:
+                    continue
+                subg = subgraph(ansg, vcc)
+                if count > 1:
+                    d += count
+                if count == 1:
+                    d += 1
+                ansg = subg
+                ansgNodes = set(ansg.nodes())
+                break
+        else:
+            if count > 1:
+                for i in range(count):
+                    tmp = red.pop()
+                    redSet.remove(tmp)
+
+            step += 1
+            if step == 4 and count > 2:
+                step = 0
+                count = max(count // 2, 2)
+
+    return ansg, anssize, d
+
+def update(ccid, dependencyMap):
+    ccs = list(nx.connected_components(dependencyMap))
+
+    for i in range(len(ccs)):
+        cc = ccs[i]
+        for v in cc:
+            ccid[v] = str(i)
+    return
+
+
+if __name__ == '__main__':
+    starttime = time.time()
+    for k in [4, 8, 12, 16, 20]:
+        for querysize in [2, 4, 8, 16]:
+            starttime = time.time()
+            finalsize = 0
+            density = 0
+            finald = 0
+            for _ in range(10):
+                readfile = open('dataEpoch//EC_power_' + str(k) + '.txt', "r+")
+                kedges = eval(readfile.readline())
+                subg = nx.Graph()
+                subg.add_edges_from(kedges)
+                print(len(subg))
+                print('query size', querysize)
+
+                ansG = subg
+
+                denominator = 0
+                if querysize > len(subg): continue
+                X = set(random.sample(list(subg.nodes()), querysize))
+                finalsizestep = len(subg)
+                aved = 0
+                for count in range(7):
+                    d = 0
+                    ans, anssize, d = MinimalSubgraph(copy.deepcopy(subg), k, d, X)
+                    print("d=", d)
+                    denominator += 1
+                    aved += d
+                    if len(ans) < len(ansG):
+                        ansG = ans
+                    finalsizestep = min(finalsizestep, anssize)
+                    if anssize == k + 1:
+                        break
+                finalsize += finalsizestep
+                finald += aved / denominator
+                density += nx.density(ansG)
+
+            endtime = time.time()
+            print("k=", k)
+            print("querysize=", querysize)
+            print("size=", finalsize / 10)
+            print("d=", finald / 10)
+            print("density", density / 10)
+            print("ave time=", (endtime - starttime) / 10)
+            print("=====================")
